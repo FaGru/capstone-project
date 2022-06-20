c869af28bb8420f16c39d808e95d9d38f735e5f2
Fabian Gruber <fabian.gruber93@gmx.de>
2022-06-20 20:00:55 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,14 @@ app.use("/api/userdata", require("./routes/userdataRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
 
 
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// // Serve frontend
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html")));
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+//   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html")));
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
 
 app.use(errorHandler);
 
--- frontend/src/hooks/backendStore.js
+++ frontend/src/hooks/backendStore.js
@@ -10,7 +10,7 @@ const backendStore = create((set, get) => ({
   userLoginInformation: userLoginInformation,
   isError: '',
   isLoading: false,
-  API_URL: process.env.REACT_APP_API_URL || '/api/user/',
+  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api/user/',
 
   register: async formData => {
     set({ isLoading: true });
