88d116695058b934d114257d7d4497d15b1d7656
Fabian Gruber <fabian.gruber93@gmx.de>
2022-06-20 20:29:19 +0200

    test

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
