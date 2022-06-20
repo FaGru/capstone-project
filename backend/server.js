7a79df6a32a60eabddc7629ef100d6f79dfb4a9f
Fabian Gruber <fabian.gruber93@gmx.de>
2022-06-20 19:16:55 +0200

    Changed api url in the backend store

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,36 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use("/api/userdata", require("./routes/userdataRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
 
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html")));
 } else {
-  app.get('/', (req, res) => res.send('Please set to production'));
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
 app.use(errorHandler);
