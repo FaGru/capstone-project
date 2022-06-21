a87cd2b496be4d90d05b705f817a8a7940c93637
Fabian Gruber <fabian.gruber93@gmx.de>
2022-06-21 13:20:15 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+console.log("hallo");
 connectDB();
 
 app.use("/api/userdata", require("./routes/userdataRoutes"));
