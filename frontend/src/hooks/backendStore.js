a13be503fa8fa1031f4c94d66dc1cbf64dc7ff50
Fabian Gruber <fabian.gruber93@gmx.de>
2022-06-22 15:48:45 +0200

    test

--- frontend/src/hooks/backendStore.js
+++ frontend/src/hooks/backendStore.js
@@ -41,7 +41,6 @@ const backendStore = create((set, get) => ({
     set({ isLoading: true });
     set({ isError: '' });
     const API_URL = get().API_URL + '/login';
-    console.log(API_URL);
     try {
       const response = await axios.post(API_URL, formData);
       if (response.data) {
