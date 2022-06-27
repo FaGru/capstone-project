e1fefa14f42787fa43733a830e8862606f70f0ff
Fabian Gruber <fabian.gruber93@gmx.de>
2022-06-27 10:56:38 +0200

    test

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 connectDB();
 
 app.use(errorHandler);
